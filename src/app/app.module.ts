a82f700015a8f4503a58e0d38a5697a8dd29ce24
Kirill Spesivtsev <ksby8819@gmail.com>
2024-12-09 12:18:11 +0100

    Added components for posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsModule } from './features/posts/posts.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    PostsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/features/posts/posts.module.ts
+++ src/app/features/posts/posts.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { PostsListComponent } from './posts-list/posts-list.component';
+import { PostComponent } from './post/post.component';
 
 
 @NgModule({
   declarations: [
+    PostsListComponent,
+    PostComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
   ],
   exports: [
+    PostsListComponent,
+    PostComponent
   ]
 })
-export class ShopModule { }
+export class PostsModule { }
